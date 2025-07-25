454922ef8501813f8a86107e1fd843bf15f9a5aa
Wang Haoyang <haoyangw@gmail.com>
2025-07-26 00:31:23 +0800

    Revert "soc: rockchip: opp_select: Implement rockchip_opp_set_low_length()"
    This causes compilation error on Linux 5.10, so it's not
      suitable for this kernel.
    
    This reverts commit d9c2dddedcf72bb04624cc01b9da8fbb5c48265f.

--- include/soc/rockchip/rockchip_opp_select.h
+++ include/soc/rockchip/rockchip_opp_select.h
@@ -130,8 +130,6 @@ int rockchip_set_intermediate_rate(struct device *dev,
 				   struct clk *clk, unsigned long old_freq,
 				   unsigned long new_freq, bool is_scaling_up,
 				   bool is_set_clk);
-int rockchip_opp_set_low_length(struct device *dev, struct device_node *np,
-				struct rockchip_opp_info *opp_info);
 int rockchip_init_opp_table(struct device *dev,
 			    struct rockchip_opp_info *info,
 			    char *lkg_name, char *reg_name);
@@ -267,13 +265,6 @@ rockchip_set_intermediate_rate(struct device *dev,
 			       struct clk *clk, unsigned long old_freq,
 			       unsigned long new_freq, bool is_scaling_up,
 			       bool is_set_clk)
-{
-	return -EOPNOTSUPP;
-}
-
-static inline int
-rockchip_opp_set_low_length(struct device *dev, struct device_node *np,
-			    struct rockchip_opp_info *opp_info)
 {
 	return -EOPNOTSUPP;
 }
